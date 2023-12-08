37ef0359ba341f1aa0cd73aa2c18d58ab6da5a19
Anjan K.C. <kcanjan2020@gmail.com>
2023-12-08 20:53:49 +0545

    convert html file to pdf using html-pdf

--- src/controller/htmlToPdfController.js
+++ src/controller/htmlToPdfController.js
@@ -1,14 +1,29 @@
-export let uploadMultipleFile = async (req, res, next) => {
+import fs, { link } from "fs";
+import pdf from "html-pdf";
+export let uploadMultipleFile = async (req, res) => {
   // console.log(req.files);
   try {
     let links = req.files.map((value, i) => {
       return `http://localhost:8000/${value.filename}`;
     });
+    //convert uploaded html file to pdf
+    let output = Date.now() + "output.pdf";
+    let filepath = req.files.map((value, i) => {
+      return value.path;
+    });
+    //console.log(...filepath);
+    let html = fs.readFileSync(...filepath, "utf8");
+    let options = { format: "Letter" };
+
+    pdf.create(html, options).toFile(`./public/${output}`, function (err, res) {
+      if (err) return console.log(err);
+      console.log(res);
+    });
 
     res.json({
       success: true,
-      message: "File uploaded Successfully",
       originalHtmlFileUrl: links,
+      message: "File uploaded Successfully",
     });
   } catch (error) {
     res.json({
